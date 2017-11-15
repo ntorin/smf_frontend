6aae005dc0648a402edadcecc46735b5b35a4f7d
salad <kevinlu98@gmail.com>
2017-11-14 23:47:28 -0500

    edited profile view, added editprofile view, added advancedsearch view

--- src/screens/pages/bbs/AdvancedSearch.js
+++ src/screens/pages/bbs/AdvancedSearch.js
@@ -1,17 +1,80 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import BaseStyles from 'helpers/styles.js';
+import React, {Component} from 'react';
+import { StyleSheet, View, Text, TextInput} from 'react-native';
+import Button from 'components/Button';
+import BaseStyles, { PrimaryColor } from 'helpers/styles.js';
+import DatePicker from 'react-native-datepicker'
 
 class AdvancedSearch extends React.Component {
 
     constructor(props){
         super(props);
+        this.state = {dateFrom:"",dateTo:""}
     }
 
     render(){
         return(
             <View style={BaseStyles.container}>
-            
+                <View style={layout.textPanel}>
+                    <TextInput style={layout.inputBar}
+                        placeholder={'🔎 Topic...'}
+                        selectionColor={PrimaryColor}
+                        textAlign='center'
+                        autoCorrect={false}
+                        autoCapitalize={'none'}/>
+                </View>
+                <View style={layout.textPanel}>
+                    <TextInput style={layout.inputBar}
+                        placeholder={'🔎 Author...'}
+                        selectionColor={PrimaryColor}
+                        textAlign='center'
+                        autoCorrect={false}
+                        autoCapitalize={'none'}/>
+                </View>
+                <View style={layout.textPanel}>
+                    <TextInput style={layout.inputBar}
+                        placeholder={'🔎 Tags...'}
+                        selectionColor={PrimaryColor}
+                        textAlign='center'
+                        autoCorrect={false}
+                        autoCapitalize={'none'}/>
+                </View>
+                <View style={layout.textPanel}>
+                    <TextInput style={layout.inputBar}
+                        placeholder={'🔎 Keywords...'}
+                        selectionColor={PrimaryColor}
+                        textAlign='center'
+                        autoCorrect={false}
+                        autoCapitalize={'none'}/>
+                </View>
+                <View style={layout.textPanel}>
+                    <DatePicker
+                        style={layout.inputBar}
+                        mode="date"
+                        date={this.state.dateFrom}
+                        placeholder="Date From"
+                        format="MM-DD-YYYY"
+                        minDate="01-01-2001"
+                        maxDate="12-30-2100"
+                        confirmBtnText="Confirm"
+                        cancelBtnText="Cancel"
+                        onDateChange={(date) => {this.setState({dateFrom: date})}}
+                    />
+                    <DatePicker
+                        style={layout.inputBar}
+                        mode="date"
+                        date={this.state.dateTo}
+                        placeholder="Date To"
+                        format="MM-DD-YYYY"
+                        minDate="01-01-2001"
+                        maxDate="12-30-2100"
+                        confirmBtnText="Confirm"
+                        cancelBtnText="Cancel"
+                        onDateChange={(date) => {this.setState({dateTo: date})}}
+                    />
+                </View>
+                <View style={{padding: 8}}>
+                    <Button>Search</Button>
+                </View>
             </View>
         )
     }
@@ -22,7 +85,17 @@ const styles = StyleSheet.create({
 });
 
 const layout = StyleSheet.create({
-
+    textPanel: {
+        flexDirection: 'row'
+    },
+    inputBar: {
+        flex: 10
+    },
+    text:   {
+        fontSize: 20,
+        flex: 4,
+        textAlignVertical: 'center'
+    }
 });
 
 export default AdvancedSearch;
--- src/screens/pages/profile/EditProfile.js
+++ src/screens/pages/profile/EditProfile.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import BaseStyles from 'helpers/styles.js';
+import { StyleSheet, View, Text, TextInput} from 'react-native';
+import Button from 'components/Button';
+import BaseStyles, { PrimaryColor } from 'helpers/styles.js';
 
 class EditProfile extends React.Component {
 
@@ -11,7 +12,35 @@ class EditProfile extends React.Component {
     render(){
         return(
             <View style={BaseStyles.container}>
-            
+                <View style={layout.textPanel}>
+                    <Text style={layout.text}>Handle: </Text>
+                    <TextInput style={layout.inputBar}
+                        selectionColor={PrimaryColor}
+                        textAlign='center'
+                        autoCorrect={false}
+                        autoCapitalize={'none'}/>
+                </View>
+                <View style={layout.textPanel}>
+                    <Text style={layout.text}>Description: </Text>
+                    <TextInput style={layout.inputBar}
+                        selectionColor={PrimaryColor}
+                        textAlign='center'
+                        autoCorrect={false}
+                        autoCapitalize={'none'}/>
+                </View>
+                <View style={layout.textPanel}>
+                    <Text style={layout.text}>Birthday: </Text>
+                    <TextInput style={layout.inputBar}
+                        selectionColor={PrimaryColor}
+                        textAlign='center'
+                        autoCorrect={false}
+                        autoCapitalize={'none'}/>
+                </View>
+                <View style={{padding: 8}}>
+                    <Button>
+                        Save
+                    </Button>
+                </View>
             </View>
         )
     }
@@ -22,7 +51,17 @@ const styles = StyleSheet.create({
 });
 
 const layout = StyleSheet.create({
-
+    textPanel: {
+        flexDirection: 'row'
+    },
+    inputBar: {
+        flex: 10
+    },
+    text:   {
+        fontSize: 20,
+        flex: 4,
+        textAlignVertical: 'center'
+    }
 });
 
 export default EditProfile;
